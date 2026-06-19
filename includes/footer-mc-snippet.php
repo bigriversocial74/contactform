b4394792db0af291cf04ad222d15ae6541f5dbf6
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-18 17:00:50 -0700

    Add operations asset snippet

---
+++ includes/footer-mc-snippet.php
@@ -0,0 +1,5 @@
+<?php
+$page_scripts[] = '/assets/js/admin-mc-operations.js';
+$page_scripts[] = '/assets/js/admin-mc-inspector.js';
+$page_scripts[] = '/assets/js/admin-mc-workflow.js';
+$late_styles[] = '/assets/css/admin-merchant-catalog-drawer.css';
