e9f4d162f95238bc1b56d4e6091dd316744a9cd4
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-20 02:47:50 -0700

    Stage F: load merchant Stripe Connect controls

--- merchant-payments.php
+++ merchant-payments.php
@@ -5,7 +5,7 @@ $page_title='Payments and Reconciliation | Microgifter';
 $page_section='merchant';
 $header_mode='account';
 $page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-payments.css'];
-$page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-payments.js'];
+$page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-payments.js','/assets/js/merchant-connect.js'];
 $merchantView='payments';
 require __DIR__.'/includes/header.php';
 require __DIR__.'/includes/merchant-workspace.php';
