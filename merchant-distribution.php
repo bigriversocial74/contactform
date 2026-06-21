7cb0fad0311f98dfc2cc122023bf87cb10c77dee
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-21 16:29:39 -0700

    Route merchant developer API through distribution shell

--- merchant-distribution.php
+++ merchant-distribution.php
@@ -1,12 +1,12 @@
 <?php
 declare(strict_types=1);
 require_once __DIR__.'/includes/app.php';
-$page_title='Merchant Distribution | Microgifter';
+$merchantView = isset($_GET['developer_api']) ? 'developer_api' : 'distribution';
+$page_title = $merchantView === 'developer_api' ? 'Developer API | Microgifter' : 'Merchant Distribution | Microgifter';
 $page_section='merchant';
 $header_mode='account';
 $page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-distribution.css'];
 $page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-distribution.js'];
-$merchantView='distribution';
 require __DIR__.'/includes/header.php';
 require __DIR__.'/includes/merchant-workspace.php';
 require __DIR__.'/includes/footer.php';
