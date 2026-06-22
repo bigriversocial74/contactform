6d9164e336bb57b6d86f9ea47f7889a24ec06262
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-22 16:06:35 -0700

    Add Local Quest Rewards config example

---
+++ examples/local-quest-rewards/config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'app_name' => 'Local Quest Rewards',
+    'base_url' => 'https://microgifter.com',
+    'api_key' => 'mg_test_replace_with_server_side_key',
+    'default_program_id' => 'dist_prog_replace_me',
+    'default_template_id' => 'tmpl_replace_me',
+    'webhook_secret' => 'replace_with_rotated_webhook_signing_value',
+    'mode' => 'sandbox',
+    'demo_external_user_id' => 'quester-9001',
+];
