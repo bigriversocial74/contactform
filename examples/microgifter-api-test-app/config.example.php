c813a0b39ed678537b5c1817a987a14c2b24a6c3
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-22 15:49:49 -0700

    Add Microgifter API test app config example

---
+++ examples/microgifter-api-test-app/config.example.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'base_url' => 'https://microgifter.com',
+    'api_key' => 'mg_test_replace_with_server_side_key',
+    'program_id' => 'dist_prog_replace_me',
+    'template_id' => 'tmpl_replace_me',
+    'webhook_secret' => 'replace_with_rotated_webhook_signing_value',
+    'default_external_user_id' => 'player-9001',
+    'default_external_event_id' => 'achievement-1001',
+];
