a9418d81434865ecc87c2da73e48c648cdc0de5b
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-23 04:11:40 -0700

    Add merchant reward templates route

---
+++ merchant-reward-templates.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/includes/app.php';
+$page_title = 'Reward Templates | Microgifter';
+$page_section = 'merchant';
+$header_mode = 'account';
+$page_styles = ['/assets/css/merchant-workspace.css'];
+$page_scripts = ['/assets/js/merchant-workspace.js'];
+$merchantView = 'reward_templates';
+require __DIR__ . '/includes/header.php';
+require __DIR__ . '/includes/merchant-workspace.php';
+require __DIR__ . '/includes/footer.php';
