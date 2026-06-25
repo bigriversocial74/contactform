a3d1b3b7f082dbfb3303f2d974b9219ca40df614
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-24 17:52:41 -0700

    Port Stage 12 Merchant CRM page

---
+++ merchant-crm.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/includes/app.php';
+$page_title = 'Merchant CRM | Microgifter';
+$page_section = 'merchant';
+$header_mode = 'account';
+$page_styles = ['/assets/css/merchant-workspace.css','/assets/css/merchant-crm.css','/assets/css/communications.css'];
+$page_scripts = ['/assets/js/merchant-workspace.js'];
+$merchantView = 'merchant_crm';
+require __DIR__ . '/includes/header.php';
+require __DIR__ . '/includes/merchant-workspace.php';
+echo '<script src="/assets/js/merchant-crm.js" defer></script>';
+echo '<script src="/assets/js/merchant-crm-messages.js" defer></script>';
+require __DIR__ . '/includes/footer.php';
