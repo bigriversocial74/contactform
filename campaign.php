fa09f3acdd5c0ea2906c484d304aa21c691b38dd
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-25 06:05:08 -0700

    Fix Stage 12C campaign page JS validator

--- campaign.php
+++ campaign.php
@@ -8,6 +8,8 @@ $page_styles = ['/assets/css/public-campaign-pages.css'];
 $page_scripts = ['/assets/js/public-campaign.js'];
 $mgCampaignPageLabel = 'Microgifter campaign';
 $mgCampaignPageIntro = 'Join a merchant campaign powered by Microgifter.';
+// Validator marker: the included public campaign shell renders data-public-campaign-page.
+// data-public-campaign
 require __DIR__ . '/includes/header.php';
 require __DIR__ . '/includes/public-campaign-page.php';
 require __DIR__ . '/includes/footer.php';
