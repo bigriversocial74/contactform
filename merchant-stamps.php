ecec3011ede323e7f8e87177422b188c6d34f832
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-26 07:51:07 -0700

    Load stamp ledger history styles

--- merchant-stamps.php
+++ merchant-stamps.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/includes/app.php';
 $page_title='Stamp Ledger | Microgifter';
 $page_section='merchant';
 $header_mode='account';
-$page_styles=['/assets/css/merchant-workspace.css','/assets/css/stamp-ledger.css'];
+$page_styles=['/assets/css/merchant-workspace.css','/assets/css/stamp-ledger.css','/assets/css/merchant-stamps-ledger.css'];
 $page_scripts=['/assets/js/merchant-workspace.js'];
 $merchantView='stamps';
 require __DIR__.'/includes/header.php';
