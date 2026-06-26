914390d35dae4673508c0c277f6630a801adc823
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-26 07:52:17 -0700

    Load DAVE credit reserve UI

--- account-market-shares.php
+++ account-market-shares.php
@@ -4,7 +4,7 @@ const MG_ACCOUNT_VIEW = 'share_market';
 ob_start();
 include __DIR__ . '/account.php';
 $html = (string)ob_get_clean();
-$script = '<script src="/assets/js/dave-share-market-state.js"></script><script src="/assets/js/dave-share-market-review-feedback.js"></script>';
+$script = '<script src="/assets/js/dave-share-market-state.js"></script><script src="/assets/js/dave-share-market-review-feedback.js"></script><script src="/assets/js/dave-share-market-credit-reserve.js"></script>';
 
 if (stripos($html, '</body>') !== false) {
     echo str_ireplace('</body>', $script . '</body>', $html);
