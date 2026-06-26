8839533dc4d3024e7d59c771605454e9f1c1ae33
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-25 18:42:33 -0700

    Register Buy-In Share Market migration

--- config/migrations.php
+++ config/migrations.php
@@ -104,6 +104,7 @@ return [
         'stage_19_ai_provider_models.sql',
         'stage_19b_ai_permission_guard.sql',
         'stage_19_merchant_market_snapshots.sql',
+        'stage_20_buy_in_share_market.sql',
         'stage_v1_release_trigger_portability.sql',
     ],
 
