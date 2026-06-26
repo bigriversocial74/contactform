a7c7a61ebd339f904807b9fd8a67a79709dab7cc
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-26 11:40:34 -0700

    Restore full legacy merchant sidebar markers

--- microgifter-main/includes/agent-sidebar.php
+++ microgifter-main/includes/agent-sidebar.php
@@ -20,5 +20,5 @@ $agentSidebarNav = [
     <?php endforeach; ?>
   </nav>
 </aside>
-<div class="mg-merchant-side-actions" hidden aria-hidden="true"><a href="/messages.php">Messages</a><a href="/merchant-locations.php">Locations</a><a class="mg-merchant-side-action is-primary" href="/build.php">Create gift</a></div>
+<div class="mg-merchant-side-actions" hidden aria-hidden="true"><a href="/messages.php">Messages</a><a href="/merchant-locations.php">Locations</a><a href="/merchant-products.php">Products &amp; offers</a><a href="/merchant-pppm.php">Orders &amp; redemptions</a><a href="/merchant-settings.php">Merchant settings</a><a class="mg-merchant-side-action is-primary" href="/build.php">Create gift</a></div>
 <div data-scanner-modal data-scanner-api="/api/merchant/scanner-claim.php" hidden aria-hidden="true"></div>
