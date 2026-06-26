5b31392faf0af1ddcf35fc2b32a56da6ccb9c775
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-26 08:45:36 -0700

    Load merchant locations redesign styles

--- merchant-locations.php
+++ merchant-locations.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/includes/app.php';
 $page_title='Merchant Locations | Microgifter';
 $page_section='merchant';
 $header_mode='account';
-$page_styles=['/assets/css/merchant-workspace.css'];
+$page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-locations-redemption.css'];
 $page_scripts=['/assets/js/merchant-workspace.js'];
 $merchantView='locations';
 
