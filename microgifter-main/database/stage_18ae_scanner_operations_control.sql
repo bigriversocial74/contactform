85cefc62647ac4f7868c75b3d2b15f82c494ee34
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-27 01:50:34 -0700

    Mirror scanner operations control migration reference

---
+++ microgifter-main/database/stage_18ae_scanner_operations_control.sql
@@ -0,0 +1,3 @@
+-- Stage 18AE Scanner Operations Control
+-- Mirror note: canonical SQL is database/stage_18ae_scanner_operations_control.sql
+SOURCE ../../database/stage_18ae_scanner_operations_control.sql;
