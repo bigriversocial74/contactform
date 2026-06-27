768707086f0d36c09895b409e1a346682841d30e
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-27 10:46:35 -0700

    Inject subscription activation status before footer close

--- account-subscriptions.php
+++ account-subscriptions.php
@@ -1,5 +1,11 @@
 <?php
 define('MG_ACCOUNT_VIEW', 'subscriptions');
+ob_start();
 require __DIR__ . '/account.php';
-?>
-<script src="/assets/js/subscription-activation-status.js"></script>
+$page = ob_get_clean();
+$script = '<script src="/assets/js/subscription-activation-status.js"></script>';
+if (strpos($page, '</body>') !== false) {
+    echo str_replace('</body>', $script . '</body>', $page);
+} else {
+    echo $page . $script;
+}
