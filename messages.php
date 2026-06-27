d6bfd35e0ec5c59eea01fc31b6461358a134d164
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-27 15:17:38 -0700

    Load messages sidebar redesign overrides

--- messages.php
+++ messages.php
@@ -5,7 +5,7 @@ $page_title='Messages | Microgifter';
 $page_section='agent';
 $header_mode='agent';
 $agent_tab='messages';
-$page_styles=['/assets/css/communications.css','/assets/css/messages-source-metadata.css','/assets/css/message-delivery-proof.css'];
+$page_styles=['/assets/css/communications.css','/assets/css/messages-source-metadata.css','/assets/css/message-delivery-proof.css','/assets/css/messages-redesign.css'];
 $page_scripts=['/assets/js/messages-center.js'];
 require __DIR__ . '/includes/header.php';
 ?>
