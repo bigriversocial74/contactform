992687160b6ff70d29d75bb05a63fc3298277808
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-28 07:54:05 -0700

    Add Developer API tabbed redesign assets

--- merchant-distribution.php
+++ merchant-distribution.php
@@ -1,12 +1,19 @@
 <?php
 declare(strict_types=1);
 require_once __DIR__.'/includes/app.php';
-$merchantView = isset($_GET['developer_api']) ? 'developer_api' : 'distribution';
+$isDeveloperApi = isset($_GET['developer_api']);
+$merchantView = $isDeveloperApi ? 'developer_api' : 'distribution';
 $page_title = $merchantView === 'developer_api' ? 'Developer API | Microgifter' : 'Merchant Distribution | Microgifter';
 $page_section='merchant';
 $header_mode='account';
 $page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-distribution.css'];
+if ($isDeveloperApi) {
+    $page_styles[] = '/assets/css/merchant-developer-api-redesign.css';
+}
 $page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-distribution.js'];
+if ($isDeveloperApi) {
+    $page_scripts[] = '/assets/js/merchant-developer-api-tabs.js';
+}
 require __DIR__.'/includes/header.php';
 require __DIR__.'/includes/merchant-workspace.php';
 require __DIR__.'/includes/footer.php';
