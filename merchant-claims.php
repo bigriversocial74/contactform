81a89b7272eefb4b4e24a800b1a86c876c9711d4
bigriversocial74 <bigriversocial74@gmail.com>
2026-06-29 07:15:17 -0700

    Load Store Health completion events on claims

--- merchant-claims.php
+++ merchant-claims.php
@@ -5,7 +5,7 @@ $page_title='Merchant Claims | Microgifter';
 $page_section='merchant';
 $header_mode='account';
 $page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-claims.css'];
-$page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-claims.js'];
+$page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-claims.js','/assets/js/store-health-completion-events.js'];
 $merchantView='claims';
 require __DIR__.'/includes/header.php';
 require __DIR__.'/includes/merchant-workspace.php';
