5de4717224e3c5d4c9545af1a008b6e2db6820c1
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-02 13:00:17 -0700

    Add merchant overview dashboard shell

---
+++ includes/merchant-overview-dashboard.php
@@ -0,0 +1,7 @@
+<?php
+ declare(strict_types=1);
+?>
+<section class="mg-merchant-overview-dashboard" data-merchant-overview-dashboard>
+  <h1>Welcome back, <span data-merchant-name>Merchant</span></h1>
+  <div data-merchant-kpis></div>
+</section>
