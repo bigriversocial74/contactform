5ce3e9ac21344cec878d3e18f7a4a07e2771ba85
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-02 05:28:23 -0700

    Add Customer Refund enum migration

--- database/customer_refund_campaign_type_note.sql
+++ database/customer_refund_campaign_type_note.sql
@@ -1,2 +1,5 @@
--- Customer Refund campaign type migration placeholder.
-SELECT 1;
+-- Add Customer Refund as an internal campaign type.
+-- Required because campaigns.campaign_type is an ENUM.
+
+ALTER TABLE campaigns
+  CHANGE campaign_type campaign_type ENUM('newsletter_signup','contest_giveaway','qr_reward_drop','referral_reward','birthday_vip','agent_offer','customer_refund') NOT NULL;
