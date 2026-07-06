7ace665a53876eeba30e9f9d77a509b37d23a71a
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-05 17:12:58 -0700

    Simplify file diagnostics read-only assertions

--- tests/phpunit/AgenticIndexOnboardingTest.php
+++ tests/phpunit/AgenticIndexOnboardingTest.php
@@ -54,9 +54,8 @@ final class AgenticIndexOnboardingTest extends TestCase
 
         self::assertStringContainsString("'protected_active'", $endpoint);
         self::assertStringContainsString("'legacy_candidate'", $endpoint);
-        self::assertStringContainsString('$deleteReady = array_values(array_filter', $endpoint);
-        self::assertStringContainsString("'delete_ready' => $deleteReady", $endpoint);
         self::assertStringContainsString("'read_only' => true", $endpoint);
+        self::assertStringContainsString('mg_legacy_file_diag_scan_references', $endpoint);
     }
 
     public function testHeaderProvidesPresentationAndLearnMoreControls(): void
