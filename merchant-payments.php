f9800771f0227b931d63e7a13349800656a96f2d
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-11 14:39:47 -0700

    Load Stripe Connect merchant styles

--- merchant-payments.php
+++ merchant-payments.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/includes/app.php';
 $page_title='Payments and Reconciliation | Microgifter';
 $page_section='merchant';
 $header_mode='account';
-$page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-payments.css'];
+$page_styles=['/assets/css/merchant-workspace.css','/assets/css/merchant-payments.css','/assets/css/merchant-stripe-connect.css'];
 $page_scripts=['/assets/js/merchant-workspace.js','/assets/js/merchant-payments.js'];
 $merchantView='payments';
 require __DIR__.'/includes/header.php';
