1a27939bf6916b744a521ca8f6f42a40e734ab3b
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-11 15:36:44 -0700

    Adapt post composer to fullscreen create center

--- includes/header-components/post-composer-modal.php
+++ includes/header-components/post-composer-modal.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 ?>
+<link rel="stylesheet" href="/assets/css/create-center-inline.css">
 <div class="mg-post-composer-modal" id="mg-post-composer-modal" data-global-post-composer hidden aria-hidden="true">
   <button class="mg-post-composer-backdrop" type="button" data-global-post-composer-close aria-label="Close post composer"></button>
   <section class="mg-post-composer-dialog" role="dialog" aria-modal="true" aria-labelledby="mg-feed-composer-title-global" tabindex="-1">
+    <button class="mg-post-composer-x" type="button" data-global-post-composer-close aria-label="Close post composer">×</button>
     <?php
     $post_composer_id_suffix = 'global';
     $post_composer_hidden = false;
@@ -11,3 +13,5 @@ declare(strict_types=1);
     ?>
   </section>
 </div>
+<script src="/assets/js/create-center-inline.js" defer></script>
+<script src="/assets/js/create-center-post-success.js" defer></script>
