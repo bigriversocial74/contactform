fa6e8d5b4d2db185c5a2713aee8837e19080e1cc
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-11 18:08:14 -0700

    Load unified create-center mobile post styles

--- includes/header-components/post-composer-modal.php
+++ includes/header-components/post-composer-modal.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 ?>
 <link rel="stylesheet" href="/assets/css/create-center-inline.css">
+<link rel="stylesheet" href="/assets/css/create-center-mobile-post-unified.css">
 <script src="/assets/js/create-center-inline.js" defer></script>
 <script src="/assets/js/create-center-storefront-preserve.js" defer></script>
 <script src="/assets/js/create-center-post-inline.js" defer></script>
