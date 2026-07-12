42997e456e74500a1c75b1ba330a801039dfbd0d
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-11 20:56:33 -0700

    Bump CRM contact layout asset version

--- merchant-crm.php
+++ merchant-crm.php
@@ -20,7 +20,7 @@ $page_styles = [
     '/assets/css/merchant-crm-contacts-clean.css',
     '/assets/css/merchant-crm-contact-link-polish.css?v=3.0.0',
     '/assets/css/merchant-crm-layout-stability.css?v=1.0.0',
-    '/assets/css/merchant-crm-contacts-only.css?v=1.0.0',
+    '/assets/css/merchant-crm-contacts-only.css?v=1.1.0',
 ];
 
 $page_scripts = [
