ecdead295d961219e98b39e203e447f11d38062d
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-13 18:22:23 -0700

    Add Mailchimp contacts service loader

---
+++ includes/integrations/mailchimp-contacts.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/mailchimp-contact-core.php';
+require_once __DIR__ . '/mailchimp-contact-import.php';
+require_once __DIR__ . '/mailchimp-contact-sync.php';
