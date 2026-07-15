48765a38505d76893d93ebce91ac16afa93254a6
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-15 14:11:42 -0700

    Return account gift cards from Personal Agent chat

--- api/user-agent/chat.php
+++ api/user-agent/chat.php
@@ -9,7 +9,7 @@ $input = mg_input();
 mg_require_csrf_for_write($input);
 
 mg_user_agent_api_run(
-    static fn(): array => mg_personal_agent_chat_with_marketplace_response(
+    static fn(): array => mg_personal_agent_chat_with_account_gift_response(
         mg_db(),
         (int) $user['id'],
         $input
