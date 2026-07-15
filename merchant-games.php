3a95b18df5526ddf5f5fe53398849bc9d1cd7f4b
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-14 23:47:00 -0700

    Link Hosted Games to release history

--- merchant-games.php
+++ merchant-games.php
@@ -18,6 +18,7 @@ $page_scripts = [
     '/assets/js/merchant-hosted-games-runtime-toggle.js?v=1.0.0',
     '/assets/js/hosted-games-cover-upload.js?v=1.0.0',
     '/assets/js/hosted-games-analytics-links.js?v=1.0.0',
+    '/assets/js/hosted-games-release-links.js?v=1.0.0',
 ];
 $merchantView = 'hosted_games';
 require __DIR__ . '/includes/header.php';
