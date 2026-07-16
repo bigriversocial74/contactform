2844ac58da1567c46cb1268694f0e9068791a9ab
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-15 21:30:55 -0700

    Decorate Personal Agent responses with opportunity attribution

--- includes/personal-agent/credit-response.php
+++ includes/personal-agent/credit-response.php
@@ -31,7 +31,7 @@ function mg_personal_agent_chat_with_credit_response(PDO $pdo, int $userId, arra
     }
 
     unset($GLOBALS['mg_last_anthropic_response']);
-    $result = mg_personal_agent_chat_with_merchant_opportunities($pdo,$userId,$input);
+    $result = mg_personal_agent_chat_with_opportunity_attribution($pdo,$userId,$input);
     $creditAfter = $creditBefore;
     $tokensUsed = ['input'=>0,'output'=>0,'total'=>0];
 
