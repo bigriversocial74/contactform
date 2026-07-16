aaf2daf7deca7ca608134438921c3757bf8f8698
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-16 11:41:36 -0700

    Load fixed and polished Create Center Post assets

--- includes/header-components/post-composer-modal.php
+++ includes/header-components/post-composer-modal.php
@@ -1,8 +1,10 @@
 <?php
 declare(strict_types=1);
 ?>
-<link rel="stylesheet" href="/assets/css/create-center-inline.css">
-<link rel="stylesheet" href="/assets/css/create-center-mobile-post-unified.css">
-<script src="/assets/js/create-center-inline.js" defer></script>
-<script src="/assets/js/create-center-storefront-preserve.js" defer></script>
-<script src="/assets/js/create-center-post-inline.js" defer></script>
+<link rel="stylesheet" href="/assets/css/create-center-inline.css?v=1.1.0">
+<link rel="stylesheet" href="/assets/css/create-center-mobile-post-unified.css?v=1.1.0">
+<link rel="stylesheet" href="/assets/css/create-center-post-professional.css?v=1.0.0">
+<script src="/assets/js/create-center-inline.js?v=1.1.0" defer></script>
+<script src="/assets/js/create-center-storefront-preserve.js?v=1.1.0" defer></script>
+<script src="/assets/js/create-center-post-inline.js?v=1.1.0" defer></script>
+<script src="/assets/js/create-center-post-trigger-fix.js?v=1.0.0" defer></script>
