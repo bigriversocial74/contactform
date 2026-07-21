f1d9b3de5da38dc1ca4b609c8e0efb8cd1b35f4e
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-20 19:05:43 -0700

    Register MCP external OAuth Phase 2A migration

--- config/migrations.php
+++ config/migrations.php
@@ -153,6 +153,7 @@ return [
         '20260720_task_agent_phase3_shortlist_v1.sql',
         '20260720_task_agent_phase4_v1.sql',
         '20260720_microgifter_mcp_automation_foundation_v1.sql',
+        '20260720_mcp_external_agent_authorization_phase2a_v1.sql',
         '20260715_ai_user_credit_packages.sql',
         '20260718_ai_credit_reconciliation_incidents.sql',
         '20260718_merchant_agent_automatic_snapshots_v1.sql',
