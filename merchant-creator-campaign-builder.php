7b1cf298597f05d6c49513e15596b3498761b6db
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-21 21:02:37 -0700

    Load isolated Phase 3 builder extension

--- merchant-creator-campaign-builder.php
+++ merchant-creator-campaign-builder.php
@@ -6,7 +6,11 @@ $page_section = 'merchant';
 $header_mode = 'account';
 $page_body_class = 'mg-creator-campaign-builder-page';
 $page_styles = ['/assets/css/merchant-workspace.css','/assets/css/merchant-creator-campaigns.css?v=2.0.0'];
-$page_scripts = ['/assets/js/merchant-creator-campaigns.js?v=2.0.0','/assets/js/merchant-creator-campaign-builder.js?v=2.0.0'];
+$page_scripts = [
+    '/assets/js/merchant-creator-campaigns.js?v=2.0.0',
+    '/assets/js/merchant-creator-campaign-builder.js?v=2.0.0',
+    '/assets/js/creator-campaign-builder-phase3.js?v=3.0.0',
+];
 $merchantView = 'creator_campaign_builder';
 require __DIR__ . '/includes/header.php';
 require __DIR__ . '/includes/merchant-workspace.php';
