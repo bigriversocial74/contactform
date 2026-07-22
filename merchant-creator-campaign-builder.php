c1833ec8744e523fe416620b3f86b478c6416bb3
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-22 11:08:05 -0700

    Load Phase 11 component layout on builder

--- merchant-creator-campaign-builder.php
+++ merchant-creator-campaign-builder.php
@@ -9,9 +9,10 @@ $page_styles = [
     '/assets/css/merchant-workspace.css',
     '/assets/css/merchant-creator-campaigns.css?v=2.0.0',
     '/assets/css/creator-campaign-ui-v11.css?v=11.0.0',
+    '/assets/css/creator-campaign-ui-v11-components.css?v=11.0.0',
 ];
 $page_scripts = [
-    '/assets/js/merchant-creator-campaigns.js?v=2.0.0',
+    '/assets/js/merchant-creator-campaigns.js?v=11.0.0',
     '/assets/js/merchant-creator-campaign-builder.js?v=2.0.0',
     '/assets/js/creator-campaign-builder-phase3.js?v=3.0.0',
 ];
