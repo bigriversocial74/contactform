64972f3f083dfc901692748bd2f1a9df74e5dbd8
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-23 18:18:37 -0700

    Add Investment service loader

---
+++ includes/investment/investment-service.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/investment-access.php';
+require_once __DIR__ . '/investment-planning.php';
+require_once __DIR__ . '/investment-management.php';
+require_once __DIR__ . '/investment-reporting.php';
